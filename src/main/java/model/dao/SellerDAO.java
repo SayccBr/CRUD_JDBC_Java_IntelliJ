ab436070ac4b45666a367da30c8c04946d943698
saycc <saymonhenrique.cassiano@gmail.com>
2025-03-04 15:20:53 -0300

    DepartmentDAO and SellerDAO interfaces

---
+++ src/main/java/model/dao/SellerDAO.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDAO {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(int id);
+    Seller findById(int id);
+    List<Seller> findAll();
+}
