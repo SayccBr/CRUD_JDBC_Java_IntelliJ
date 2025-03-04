89e0a7fa96bc17e9e06db16d306059af46f5989a
saycc <saymonhenrique.cassiano@gmail.com>
2025-03-04 15:30:38 -0300

    SellerDAOJDBC and DAOFactory

---
+++ src/main/java/model/dao/impl/SellerDAOJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDAOJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public Seller findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
